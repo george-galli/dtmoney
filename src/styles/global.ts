572ac3d7d49b093d59c5a450757d8f3e917f1880
George Galliera <galliera@uol.com.br>
2021-08-05 18:10:13 +0100

    First Commit

---
+++ src/styles/global.ts
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+	:root {
+		--background: #f0f2f5;
+		--red: #E52E4D;
+		--green: #33CC95;
+		--blue: #5429CC;
+		--blue-light: #6933FF;
+		--text-title: #363F5f;
+		--text-body: #969CB3;
+		--shape: #FFFFFF;
+	}
+
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+
+	// font-size: 16px   (Desktop)
+	html {
+		@media (max-width: 1080px) {
+			font-size: 93.75%;   // 15px
+		}
+
+		@media (max-width: 720px) {
+			font-size: 87.5%;  // 14px
+		}
+	}
+	// REM = 1rem = tamanho do font-size da pagina = 16px , no caso;
+	body {
+		background: var(--background);
+		-webkit-font-smoothing: antialiased;
+	}
+
+	body, input, button, textarea {
+		font-family: 'Poppins', sans-serif;
+		font-weight: 400;
+	}
+
+	h1, h2, h3, h4, h5, h6, strong {
+		font-weight: 600;
+	}
+
+	button {
+		cursor: pointer;
+	}
+	[disabled] {
+		opacity: 0.6;
+		cursor: not-allowed;
+	}
+`
