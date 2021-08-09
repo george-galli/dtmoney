299f6e056ba1c53765c4019ce11d032224c9d3fe
George Galliera <galliera@uol.com.br>
2021-08-09 22:59:59 +0100

    third commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,15 +2,19 @@ import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
 
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
 
-export function Header() {
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return  (
     <Container>
       <Content>
-      <img src={logoImg} alt="dt money" />
-      <button type="button">
-        Nova transacao
-      </button>
+        <img src={logoImg} alt="dt money" />
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transacao
+        </button>
+
       </Content>
     </Container>
   )
